031fe4771f7fd37db613b579e5ef6b83915a5c06
DanoR <zheung@vip.qq.com>
2017-02-08 15:04:09 +0800

    1702082

--- init.js
+++ init.js
@@ -1,7 +1,8 @@
 module.exports = ($) => {
 	for(let path of $.conf.pathDict)
 		try {
-			$.dict = $.rq(path);
+			let paths = path.split(':');
+			$.dict = paths[0]=='i' ? $.rq(paths[1]) : require(paths[1]);
 
 			break;
 		}
